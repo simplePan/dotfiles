4b7de28f477e84a2fca7d3097ee767f09d78c098
Hunter Loftis <hunter@hunterloftis.com>
2017-11-09 15:24:56 -0500

    apps

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -1,6 +1,9 @@
-# Homebrew
+# Homebrew tools and apps
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install caskroom/cask/brew-cask
+brew install heroku wget
+brew install go --cross-compile-common
+brew cask install google-chrome utorrent vlc spotify steam slack visual-studio-code 1password iterm2 unity
 
 # Node Version Manager
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
-
