ccf0338d42ee251666e6681674bec62b15fbefa9
Hunter Loftis <hunter@hunterloftis.com>
2017-11-09 14:14:42 -0500

    setup scripts

---
+++ scripts/apps.sh
@@ -0,0 +1,6 @@
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Node Version Manager
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+
