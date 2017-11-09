6f48e265da47356b05a8997d95005540957644b3
Hunter Loftis <hunter@hunterloftis.com>
2017-11-09 15:32:16 -0500

    updates to apps

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -1,9 +1,13 @@
+# Guarantee a profile
+touch ~/.bash_profile
+
+# Use the default GOPATH
+mkdir -p ~/go
+
 # Homebrew tools and apps
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install caskroom/cask/brew-cask
-brew install heroku wget
-brew install go --cross-compile-common
-brew cask install google-chrome utorrent vlc spotify steam slack visual-studio-code 1password iterm2 unity
+brew install heroku/brew/heroku wget go
+brew cask install google-chrome vlc spotify steam slack visual-studio-code 1password iterm2 unity
 
 # Node Version Manager
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
