7265180ec42da9706e593a7676605c49c2e2ea01
Hunter Loftis <hunter@hunterloftis.com>
2017-11-09 15:41:23 -0500

    use a Brewfile

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -6,8 +6,7 @@ mkdir -p ~/go
 
 # Homebrew tools and apps
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install heroku/brew/heroku wget go
-brew cask install google-chrome vlc spotify steam slack visual-studio-code 1password iterm2 unity
+brew bundle
 
 # Node Version Manager
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
